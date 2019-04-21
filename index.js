3ece11a8c7820eeb6c68323004c0a91a66806aa1
Theo Weller <weller.theodor@gmail.com>
2019-04-21 21:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ You should NOT try to redefine any of the above variables.
 
 const newID = oldID + 1000000000;
 
-function ageIsValid(currentAge) {
+const ageIsValid = function(currentAge) {
   Number.isInteger(currentAge);
 }
